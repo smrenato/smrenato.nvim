6fe7913d86a2913b46a1ddd1beff1778e780c2bb
renatorenatorenatorenatorenatorenatorenato <renatosmx@gmail.com>
2025-07-23 18:34:31 -0300

    Better page Scrolling

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,2 +1,4 @@
 -- Oil keymaps
 vim.keymap.set("n", "-", "<CMD>Oil --float<CR>", { desc = "Oil open parent directory" })
+vim.keymap.set("n", "<C-d>", "<C-d>zz", { noremap = true, silent = true, desc = "Scrolling page down and center" })
+vim.keymap.set("n", "<C-u>", "<C-u>zz", { noremap = true, silent = true, desc = "Scrolling page up and center" })
